4b7aa2ba8b4f7626c26dfc8a0c6d69610db3b559
Robbe Eyckmans <robbee14@gmail.com>
2023-12-04 20:22:24 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import benner from "@/components/benner.vue";
+import navBar from "@/components/navBar.vue";
+import SchoenLijst from "@/components/schoenLijst.vue";
+
+
+
+</script>
+
+<template>
+  <benner/>
+  <!-- <navBar/> -->
+  <schoenLijst/>
+</template>
+
+<style scoped>
+
+</style>
